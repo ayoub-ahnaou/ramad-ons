81dc67fa29eadd35f11f352f2979b3987173f4c0
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-02-20 12:03:19 +0100

    Add toast notification for session messages in recipes index view

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,5 +1,22 @@
 <x-layout>
     <main class="container mx-auto py-12">
+        @if (session('message'))
+            <div id="toast"
+                class="flex items-center gap-2 absolute top-12 right-0 pr-40 bg-green-100 text-green-700 rounded-md p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <circle cx="12" cy="12" r="10" stroke="green" />
+                    <path d="M9 12l2 2 4-4" stroke="green" />
+                </svg>
+                {{ session('message') }}
+            </div>
+        @endif
+        <script>
+            setTimeout(() => {
+                document.getElementById("toast").style.display = "none";
+            }, 2000);
+        </script>
+        
         <!-- Header -->
         <div class="mb-6">
             <h1 class="text-3xl font-bold mb-1">Recipes</h1>
@@ -12,12 +29,14 @@
                 <button class="px-4 py-2 bg-gray-600 text-white rounded-full">Tous</button>
                 <button
                     class="px-4 py-2 bg-gray-50 border text-gray-600 rounded-full hover:bg-gray-100">Recettes</button>
-                <button class="px-4 py-2 bg-gray-50 border text-gray-600 rounded-full hover:bg-gray-100">Astuces</button>
+                <button
+                    class="px-4 py-2 bg-gray-50 border text-gray-600 rounded-full hover:bg-gray-100">Astuces</button>
                 <button
                     class="px-4 py-2 bg-gray-50 border text-gray-600 rounded-full hover:bg-gray-100">Articles</button>
             </div>
 
-            <a href="{{route('recipes.create')}}" class="flex items-center gap-1 bg-gray-50 hover:bg-gray-100 px-4 rounded-full">
+            <a href="{{ route('recipes.create') }}"
+                class="flex items-center gap-1 bg-gray-50 hover:bg-gray-100 px-4 rounded-full">
                 <span class="text-sm text-gray-600">Ajouter recette</span>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
