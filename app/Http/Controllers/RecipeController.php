5adf5657560390e4ebde5a6f8cb212e1ea52ebb5
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-02-20 11:23:45 +0100

    Implement recipe pagination, validation, and image upload in RecipeController

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -12,7 +12,8 @@ class RecipeController extends Controller
      */
     public function index()
     {
-        return view("recipes.index");
+        $recipes = Recipe::query()->orderBy('created_at', 'desc')->paginate(6);
+        return view("recipes.index", compact('recipes'));
     }
 
     /**
@@ -28,15 +29,34 @@ class RecipeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'title' => ['required', 'string', 'max:100'],
+            'description' => ['required', 'string'],
+            'ingredients' => ['required', 'string'],
+            'persons' => ['required', 'integer'],
+            'time' => ['required', 'date_format:H:i'],
+            'category' => ['required', 'string'],
+            'image' => ['required', 'image'],
+        ]);
+        
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+
+            $imagePath = $image->store('images', 'public'); // 'images' is the folder, 'public' is the disk
+            $data['image'] = $imagePath;
+
+            Recipe::create($data);
+            return to_route('recipes.index')->with('success', 'Recipe created successfully!');
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Recipe $recipe)
+    public function show(string $id)
     {
-        return view("recipes.show");
+        $recipe = Recipe::query()->find($id);
+        return view("recipes.show", compact("recipe"));
     }
 
     /**
