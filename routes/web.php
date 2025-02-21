430f7bdfcc0863f7a17b8f3106acd4ee007f1e3b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-02-21 19:43:46 +0100

    Add forgotten method name in the create route for each recipes, publications and comments

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, "welcome"])->name("welcome");
 
 Route::get('publications', [PublicationController::class, 'index'])->name("publications.index");
-Route::get('publications/create', [PublicationController::class])->name('publications.create');
+Route::get('publications/create', [PublicationController::class, 'create'])->name('publications.create');
 Route::post('publications', [PublicationController::class, 'store'])->name('publications.store');
 Route::get('publications/{id}', [PublicationController::class, 'show'])->name('publications.show');
 Route::delete('publications/{id}', [PublicationController::class, 'destroy'])->name('publications.destroy');
 
 Route::get('recipes', [RecipeController::class, 'index'])->name('recipes.index');
-Route::get('recipes/create', [RecipeController::class])->name('recipes.create');
+Route::get('recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
 Route::post('recipes', [RecipeController::class, 'store'])->name('recipes.store');
 Route::get('recipes/{id}', [RecipeController::class, 'show'])->name('recipes.show');
 Route::delete('recipes/{id}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
 
-Route::get('comments/create', [CommentController::class])->name('comments.create');
+Route::get('comments/create', [CommentController::class], 'create')->name('comments.create');
 Route::post('comments', [CommentController::class, 'store'])->name('comments.store');
 Route::delete('comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
